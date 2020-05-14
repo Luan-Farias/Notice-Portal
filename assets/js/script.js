ca53daa3690b2a2d6896fe7b79400d3cc4d71002
Luan Farias <matheus.frs22123@outlook.com>
2020-05-14 10:36:52 -0300

    Completed Portal

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+var slideItem = 0;
+window.onload = function() {
+	setInterval(passarSlide, 2000);
+
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	var objs = document.getElementsByClassName("slide");
+	for(var i in objs) {
+		objs[i].style.width = slidewidth;
+	}
+}
+function passarSlide() {
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	var bolinhas = document.querySelectorAll('.bolinha');
+	bolinhas[slideItem].classList.remove('bolinha-ativa');
+	
+	if(slideItem >= 3) {
+		slideItem = 0;
+	} else {
+		slideItem++;
+	}
+
+	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	bolinhas[slideItem].classList.add('bolinha-ativa');
+}
+function mudarSlide(pos) {
+	var bolinhas = document.querySelectorAll('.bolinha');
+	bolinhas[slideItem].classList.remove('bolinha-ativa');
+	slideItem = pos;
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	bolinhas[slideItem].classList.add('bolinha-ativa');
+}
+
+function toggleMenu() {
+
+	var menu = document.getElementById("menu");
+
+	if (menu.style.display == 'none' || menu.style.display == '') {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+
+}
